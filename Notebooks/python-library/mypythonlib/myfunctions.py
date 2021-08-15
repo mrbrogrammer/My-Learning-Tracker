2851206318bb84d97534a985b181acb51e3dd5ee
Admin <admin@Simphiwes-MacBook-Pro.local>
2021-08-15 11:48:11 +0200

    Update on Python notes

---
+++ Notebooks/python-library/mypythonlib/myfunctions.py
@@ -0,0 +1,13 @@
+def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
+    
+    # Convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2,
+    lat2])
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km * 1000
